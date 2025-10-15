5528c5777184849afdcf5102aa03aec3a8d0512f
Issiins <160136271+Issiins@users.noreply.github.com>
2025-10-15 12:28:16 +0300

    Fixed movement

--- Assets/Scripts/VRCrawler.cs
+++ Assets/Scripts/VRCrawler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.XR;
 
 public class ClimbingHand : MonoBehaviour
 {
@@ -11,60 +12,82 @@ public class ClimbingHand : MonoBehaviour
     public string _climbableTag = "Climbable";
     public float _climbStrength = 0.2f;
 
-    private float _debugMoveSpeed = 0.02f;
     private Vector3 _lastPos;
     private bool _isGrabbing = false;
     private bool _canGrab = false;
     private Vector3 _grabPosition;
     private Collider _currentClimbable;
 
+    private InputDevice _controller;
+
     void Start()
     {
         _lastPos = transform.position;
+        InitializeController();
+    }
+
+    void InitializeController()
+    {
+        var handCharacteristic = _hand == HandType.Left ? InputDeviceCharacteristics.Left : InputDeviceCharacteristics.Right;
+        var devices = new System.Collections.Generic.List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Controller | handCharacteristic, devices);
+
+        if (devices.Count > 0)
+        {
+            _controller = devices[0];
+            Debug.Log($"{_hand} controller found: {_controller.name}");
+        }
     }
 
     void Update()
     {
         Vector3 move = Vector3.zero;
 
+        bool grabInput = false;
 
-        // added for testing without VR
+        if (_controller.isValid)
+        {
+            if (_controller.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerValue))
+            {
+                grabInput = triggerValue;
+            }
+        }
+
         if (_debugMode)
         {
-            bool grabInput = (_hand == HandType.Left) ? Input.GetKey(KeyCode.Q) : Input.GetKey(KeyCode.E);
+            grabInput = (_hand == HandType.Left) ? Input.GetKey(KeyCode.Q) : Input.GetKey(KeyCode.E);
 
-            if (grabInput && _canGrab && !_isGrabbing)
-            {
-                _isGrabbing = true;
-                Debug.Log("Grabbing");
-                _grabPosition = transform.position; 
-            }
-            else
-            {
-                _isGrabbing = false;
-                Debug.Log($"Not Grabbing - {_canGrab}");
-
-            }
             if (_hand == HandType.Left)
             {
-                if (Input.GetKey(KeyCode.W)) move += transform.forward * _debugMoveSpeed;
-                if (Input.GetKey(KeyCode.S)) move -= transform.forward * _debugMoveSpeed;
-                if (Input.GetKey(KeyCode.A)) move -= transform.right * _debugMoveSpeed;
-                if (Input.GetKey(KeyCode.D)) move += transform.right * _debugMoveSpeed;
-                if (Input.GetKey(KeyCode.G)) move += transform.up * _debugMoveSpeed; // up
-                if (Input.GetKey(KeyCode.H)) move -= transform.up * _debugMoveSpeed; // down
+                if (Input.GetKey(KeyCode.W)) move += transform.forward * 0.02f;
+                if (Input.GetKey(KeyCode.S)) move -= transform.forward * 0.02f;
+                if (Input.GetKey(KeyCode.A)) move -= transform.right * 0.02f;
+                if (Input.GetKey(KeyCode.D)) move += transform.right * 0.02f;
+                if (Input.GetKey(KeyCode.G)) move += transform.up * 0.02f; // up
+                if (Input.GetKey(KeyCode.H)) move -= transform.up * 0.02f; // down
             }
             else
             {
-                if (Input.GetKey(KeyCode.UpArrow)) move += transform.forward * _debugMoveSpeed;
-                if (Input.GetKey(KeyCode.DownArrow)) move -= transform.forward * _debugMoveSpeed;
-                if (Input.GetKey(KeyCode.LeftArrow)) move -= transform.right * _debugMoveSpeed;
-                if (Input.GetKey(KeyCode.RightArrow)) move += transform.right * _debugMoveSpeed;
-                if (Input.GetKey(KeyCode.Period)) move += transform.up * _debugMoveSpeed; // up
-                if (Input.GetKey(KeyCode.Comma)) move -= transform.up * _debugMoveSpeed; // down
+                if (Input.GetKey(KeyCode.UpArrow)) move += transform.forward * 0.02f;
+                if (Input.GetKey(KeyCode.DownArrow)) move -= transform.forward * 0.02f;
+                if (Input.GetKey(KeyCode.LeftArrow)) move -= transform.right * 0.02f;
+                if (Input.GetKey(KeyCode.RightArrow)) move += transform.right * 0.02f;
+                if (Input.GetKey(KeyCode.Period)) move += transform.up * 0.02f; // up
+                if (Input.GetKey(KeyCode.Comma)) move -= transform.up * 0.02f; // down
             }
 
             transform.position += move;
+        }
+
+        // Handle grabbing
+        if (grabInput && _canGrab && !_isGrabbing)
+        {
+            _isGrabbing = true;
+            _grabPosition = transform.position;
+        }
+        else if (!grabInput)
+        {
+            _isGrabbing = false;
         }
 
         if (_isGrabbing)
@@ -76,6 +99,12 @@ public class ClimbingHand : MonoBehaviour
         }
 
         _lastPos = transform.position;
+
+        // Re-initialize controller if disconnected
+        if (!_controller.isValid)
+        {
+            InitializeController();
+        }
     }
 
     void OnTriggerEnter(Collider other)
