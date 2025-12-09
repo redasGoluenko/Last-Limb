cf2503b450e4283cbfeb7c8731d25d6091c8a374
Issiins <160136271+Issiins@users.noreply.github.com>
2025-12-09 23:49:00 +0200

    added player's death

---
+++ Assets/Scripts/DeathManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathManager : MonoBehaviour
+{
+    private HealthManager _healthManager;
+    private string _deathSceneName = "Death scene";
+
+
+    void Start()
+    {
+        _healthManager = GetComponent<HealthManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_healthManager != null && _healthManager.isDead())
+        {
+            LoadDeathScene();
+        }
+    }
+    private void LoadDeathScene()
+    {
+        SceneManager.LoadScene(_deathSceneName);
+    }
+}
